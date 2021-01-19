a75bc67729490e6d87718526b997158a6e6b3d98
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-18 19:53:35 -0800

    Initial CSS for LoginPage.

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -14,39 +14,38 @@ function LoginPage(props) {
 
   return (
     <div className="login-cont">
-      <div id="cont-1">
-          <div id="fb-signin-logo">fakebook</div>
-          <div id="fb-signin-text">
-            Connect with friends and the world around you on Facebook.
-          </div>
+      <div className="login-text-cont">
+        <div id="fb-signin-logo">fakebook</div>
+        <div id="fb-signin-text">
+          Connect with friends and the world around you on Facebook.
         </div>
-        <div
-          className="login-content"
-          onClick={(event) => {
-            event.stopPropagation();
-          }}
-        >
-          <div id="title-login"> Log In: </div>
-          <input
-            id="username"
-            className="input"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            id="password"
-            className="input"
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className="error-message-login">{errorMessage}</div>
-          <div id="submit-login" onClick={() => {}}>
-            Log In
-          </div>
+      </div>
+      <div
+        className="login-content"
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        <input
+          id="username"
+          className="input"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          id="password"
+          className="input"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="error-message-login">{errorMessage}</div>
+        <div id="submit-login" onClick={() => {}}>
+          Log In
         </div>
+      </div>
     </div>
   );
 }
