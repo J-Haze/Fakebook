4438e9c66b28785cc4589ef740b6bcaffa6d111c
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-01-24 17:14:27 -0800

    Saving.

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -4,7 +4,8 @@ import "./Header.css";
 import { useHistory } from "react-router-dom";
 
 import fLogo from "../../../assets/facebook-logo-header.png";
-import logoutLogo from "../../../assets/logout.png";
+import logoutLogo from "../../../assets/logout-alt.png";
+import bellLogo from "../../../assets/bell.png";
 import friendsLogo from "../../../assets/friend.png";
 
 function Header(props) {
@@ -16,17 +17,39 @@ function Header(props) {
         <div id="header-left-menu">
           <Link
             id="fb-icon-cont"
-            className="link-header header-btn"
+            className="link-header"
             to={`/user/${props.currentUser._id}`}
           >
             <img className="fb-icon" src={fLogo} alt="fb logo" />
           </Link>
         </div>
         <div id="header-right-menu">
-          {" "}
+          <Link
+            id="friends-cont"
+            className="link hover-gray"
+            to={`/user/${props.currentUser._id}`}
+          >
+            <img
+              className="friends-icon"
+              src={friendsLogo}
+              alt="friends icon"
+            />
+          </Link>
+          <Link
+            id="bell-cont"
+            className="link hover-gray"
+            to={`/user/${props.currentUser._id}`}
+          >
+            <img
+              className="bell-icon"
+              src={bellLogo}
+              alt="notifications icon"
+            />
+          </Link>
+
           <Link
             id="logout-cont"
-            className="link header-btn"
+            className="link hover-gray"
             to={`/user/${props.currentUser._id}`}
           >
             <img className="logout-icon" src={logoutLogo} alt="logout icon" />
