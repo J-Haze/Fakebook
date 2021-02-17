2c91cffee4b9e2483e2cccd89457e1e78c18503e
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-02-16 16:44:40 -0800

    Cleaned CurrentUserPage.js

--- client/src/components/views/UserPage/PageType/CurrentUserPage/CurrentUserPage.js
+++ client/src/components/views/UserPage/PageType/CurrentUserPage/CurrentUserPage.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState, useEffect, useRef, forwardRef } from "react";
-import Axios from "axios";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
@@ -12,33 +11,16 @@ import "../../UserPage.css";
 
 function CurrentUserPage(props) {
   const [userPosts, setUserPosts] = useState([]);
-
   const [friendCount, setFriendCount] = useState(0);
-
-  //  const [editUserModalOpen, setEditUserModalOpen] = useState(true);
   const [editUserModalOpen, setEditUserModalOpen] = useState(false);
-
   const [deleteUserModalOpen, setDeleteUserModalOpen] = useState(false);
-  // const [deleteUserModalOpen, setDeleteUserModalOpen] = useState(true);
-
-  // const [refs, setRefs] = useState([]);
 
   const history = useHistory();
 
-  // console.log("props.allUsers currentUserPage", props.allUsers)
-
   useEffect(() => {
-    let userPostArray = [];
-
     let ownPosts = [];
     for (let i = 0; i < props.displayedPosts.length; i++) {
-      //   console.log(i, props.displayedPosts[i]);
-      //   console.log(props.displayedPosts.author);
-      //   console.log(props.currentUser);
       if (props.displayedPosts[i].author._id == props.currentUser._id) {
-        // console.log("match");
-        // console.log(props.displayedPosts[i].author._id);
-        // console.log(props.currentUser._id);
         if (ownPosts.length == 0 || ownPosts == undefined) {
           ownPosts = [props.displayedPosts[i]];
         } else {
@@ -46,10 +28,7 @@ function CurrentUserPage(props) {
         }
       }
     }
-    // console.log("ownPosts end", ownPosts);
-    // console.log("displayedPosts end", props.displayedPosts);
     setUserPosts(ownPosts.reverse());
-    // setDisplayedComments(res.data);
   }, [props.displayedPosts]);
 
   function calculateFriendCount() {
@@ -68,80 +47,24 @@ function CurrentUserPage(props) {
     calculateFriendCount();
   }, []);
 
-  console.log("CurrentUserPage")
-
-  console.log("FriendList", props.currentUser.friendList);
-
-
-//  useEffect(() => {
-//    const refsVar = userPosts.reduce((acc, value) => {
-//      acc[value._id] = React.createRef();
-//      return acc;
-//    }, {});
-
-//    setRefs(refs)
-//  }, [userPosts]);
-
-  // const ref = React.createRef();
-
-  // const refs = userPosts.reduce((acc, value) => {
-  //   acc[value._id] = React.createRef();
-  //   return acc;
-  // }, {});
-
-  // useEffect(() => {
-  //   // If there is a notification command then scroll to the correct ref
-
-  //   if (refs[props.refTarget]) {
-  //     console.log("yes ref", props.refTarget);
-  //     // props.ref.current.scrollIntoView({
-  //     // document.getElementById(props.refTarget).current.scrollIntoView({
-  //     //   behavior: "smooth",
-  //     //   block: "start",
-  //     // });
-  //     refs[props.refTarget].current.scrollIntoView({
-  //       behavior: "smooth",
-  //       block: "start",
-  //     });
-  //   }
-  // }, [props.refTarget]);
-
-  // const handleClick = (id) =>
-  //   refs[id].current.scrollIntoView({
-  //     behavior: "smooth",
-  //     block: "start",
-  //   });
-
-  // const refs = userPosts.reduce((acc, value) => {
-  //   acc[value._id] = React.createRef();
-  //   // acc[value._id] = useRef();
-  //   return acc;
-  // }, {});
-
   return (
     <div id="current-user-page">
       {editUserModalOpen && (
         <EditUserModal
-          // createPostModalOpen={createPostModalOpen}
           setEditUserModalOpen={setEditUserModalOpen}
           currentUser={props.currentUser}
-          // fetchPosts={fetchPosts}
         />
       )}
 
       {deleteUserModalOpen && (
         <DeleteUserModal
-          // createPostModalOpen={createPostModalOpen}
           setDeleteUserModalOpen={setDeleteUserModalOpen}
           currentUser={props.currentUser}
           setCurrentUser={props.setCurrentUser}
           isLoggedIn={props.isLoggedIn}
           setIsLoggedIn={props.setIsLoggedIn}
-          // fetchPosts={fetchPosts}
         />
       )}
-      {/* <div id="user-page-content-cont"> */}
-      {/* <div className="test"> */}
       <div id="user-info-cont">
         {props.currentUser.photo ? (
           <img
@@ -156,18 +79,7 @@ function CurrentUserPage(props) {
         <div className="user-card-username">
           {props.currentUser.firstname} {props.currentUser.lastname}
         </div>
-
         <div id="user-info-box">
-          {/* <span
-            // className="del-post-x"
-            onClick={(event) => {
-              //   event.stopPropagation();
-              //   setDeletePostModalOpen(true);
-            }}
-          >
-            Edit Profile
-          </span> */}
-
           <div id="bio-cont">
             <div className="flex">
               <div id="bio-header" className="user-info-header">
@@ -176,14 +88,12 @@ function CurrentUserPage(props) {
               <span
                 className="edit-user-prof"
                 onClick={(event) => {
-                  // event.stopPropagation();
                   setEditUserModalOpen(true);
                 }}
               >
                 Edit Profile
               </span>
             </div>
-            {/* Edit profile float */}
             <div id="bio" className="user-info-text">
               {props.currentUser.bio}
             </div>
@@ -193,7 +103,6 @@ function CurrentUserPage(props) {
             <div id="location-header" className="user-info-header">
               Location
             </div>
-            {/* Edit profile float */}
             <div id="location" className="user-info-text">
               {props.currentUser.location}
             </div>
@@ -203,7 +112,6 @@ function CurrentUserPage(props) {
             <div id="occupation-header" className="user-info-header">
               Occupation
             </div>
-            {/* Edit profile float */}
             <div id="occupation" className="user-info-text">
               {props.currentUser.occupation}
             </div>
@@ -214,9 +122,7 @@ function CurrentUserPage(props) {
             <div id="friend-header">Friends</div>
             <span
               className="see-all-friends"
-              onClick={(event) => {
-                //   event.stopPropagation();
-                //   setDeletePostModalOpen(true);
+              onClick={() => {
                 history.push(`/friends/${props.currentUser._id}`);
               }}
             >
@@ -271,7 +177,6 @@ function CurrentUserPage(props) {
           )}
         </div>
       </div>
-      {/* </div> */}
       <div id="user-post-cont">
         <div id="new-post-card">
           <div id="new-post-card-top" className="new-post-card-row">
@@ -291,22 +196,11 @@ function CurrentUserPage(props) {
               What's on your mind, {props.currentUser.firstname}?
             </div>
           </div>
-          {/* <div
-                id="new-post-card-bottom"
-                className="new-post-card-row"
-              ></div> */}
         </div>
-        {/* <div className="main-subtitle">All Posts:</div> */}
         {userPosts.map((post) =>
           post.isPublished ? (
             <Card
               key={post._id}
-              // ref={ref}
-              // ref={useRef(`ref-post-${props.post._id}`)}
-              // ref={refs[post._id]}
-              // ref={refs[post._id]}
-              // forwardRef={refs[post._id]}
-              // id={`ref-post-${post._id}`}
               post={post}
               currentUser={props.currentUser}
               fetchPosts={props.fetchPosts}
@@ -316,12 +210,10 @@ function CurrentUserPage(props) {
             ""
           )
         )}
-
         <div className="no-posts-profile">
           <div className="no-posts-header-profile">No More Posts</div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 }
