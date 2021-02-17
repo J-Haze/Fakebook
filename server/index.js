5a6c4d58af0ce0f673497f00db5b2286a7d40632
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-02-16 18:27:44 -0800

    Cleaned server index.js

--- server/index.js
+++ server/index.js
@@ -48,7 +48,6 @@ app.use(passport.session());
 
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
