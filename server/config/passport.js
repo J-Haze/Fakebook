544be5e7079b9c10404ef53bbbf30117e0be37bb
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-02-16 17:29:00 -0800

    Cleaned passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,17 +7,12 @@ const ExtractJWT = passportJWT.ExtractJwt;
 const LocalStrategy = require("passport-local").Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const extractJWT = require("passport-jwt").ExtractJwt;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const bcrypt = require("bcrypt");
 const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
 const User = require("../models/User");
 
-//Update passport part
-// JWTStrategy vs LocalStrategy ?
-
-//I think that JWT is being used and that this is being over written. Which is fine, but looks sloppy
 passport.use(
   new LocalStrategy(
     {
@@ -45,8 +40,6 @@ passport.use(
   )
 );
 
-// console.log("secretTunnel:", process.env.JWT_SECRET)
-
 passport.use(
   new JWTStrategy(
     {
@@ -60,71 +53,6 @@ passport.use(
     }
   )
 );
-
-//Make sure you go to https://developers.facebook.com/ and update the redirect url after it is deployed
-
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: keys.fbAppId,
-//       clientSecret: keys.fbAppSecret,
-//       callbackURL:
-//         process.env.NODE_ENV === "development"
-//           ? "http://localhost:3000/user/auth/facebook/callback"
-//           : "",
-//       // callbackURL: '/api/facebook/callback',
-//       profileFields: ["displayName", "photos", "email"],
-//       // profileFields: ["id", "displayName", "picture.type(large)"],
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       console.log("did it work")
-//       // User.findOne({ facebookId: profile.id }, function (err, user) {
-//       User.findOne({ facebookId: profile.id }, function (err, user) {
-//         if (err) {
-//           console.log(err);
-//           return res.json(err);
-//         }
-
-//         //If user exists on Fakebook, return no error and the user
-//         if (user) {
-//           done(null, user);
-//         }
-
-//         let name = profile.displayName.split(" ");
-
-//         if (name.length !== 2) {
-//           // failed to get a first and last name
-//           name = [profile.displayName, ""];
-//         }
-
-//         //If no fakebook user with this facebook account then create new user
-//         var newUser = new User({
-//           firstname: name[0],
-//           lastname: name[1],
-//           email: profile.emails[0].value,
-//           password: keys.fbPassword,
-//           birthDate: new Date(),
-//           gender: "other",
-//           friendList: [],
-//           photo: profile.photos ? profile.photos[0].value : "",
-//           realFacebookID: profile.id,
-//           isPublished: true,
-//         });
-
-//         newUser.save(function (err, createdUser) {
-//           if (err) {
-//             console.log("Saving error");
-//             console.log(err);
-//             return done(err);
-//           } else {
-//             res.json("Submitted");
-//             return done(err, createdUser);
-//           }
-//         });
-//       });
-//     }
-//   )
-// );
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
