21e2165230722b7921e927b3a59e1d993fa13647
J-Haze <59891689+J-Haze@users.noreply.github.com>
2021-02-16 20:57:54 -0800

    Updated server index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", auth, post_controller.get_posts);
+
 var post_controller = require("../controllers/post_controller");
 
+const auth = require("../middleware/auth");
+
 // GET - Get all posts
-router.get("/", post_controller.get_posts);
+router.get("/", auth, post_controller.get_posts);
 
 module.exports = router;
